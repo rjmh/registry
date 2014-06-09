fef7cfab475824d248897ecc355ba2b8dbb8b0b1
rjmh <john.hughes@quviq.com>
2014-06-09 11:49:07 +0200

    a

--- registry_eqc.erl
+++ registry_eqc.erl
@@ -35,10 +35,6 @@ register_pre(S) ->
 register_args(S) ->
   [name(),elements(S#state.pids)].
 
-register_pre(S,[Name,Pid]) ->
-  not lists:keymember(Name,1,S#state.regs) andalso
-  not lists:keymember(Pid,2,S#state.regs).
-
 register(Name,Pid) ->
   erlang:register(Name,Pid).
 
@@ -50,14 +46,8 @@ register_next(S,_,[Name,Pid]) ->
 unregister_args(_) ->
   [name()].
 
-unregister_pre(S,[Name]) ->
-  lists:keymember(Name,1,S#state.regs).
-
 unregister(Name) ->
   erlang:unregister(Name).
-
-unregister_next(S,_,[Name]) ->
-  S#state{regs = lists:keydelete(Name,1,S#state.regs)}.
 
 %% the property
 
