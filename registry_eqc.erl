ca92e3296c1544d396659757138b7e456042a058
rjmh <john.hughes@quviq.com>
2014-09-04 16:04:52 +0200

    foo

--- registry_eqc.erl
+++ registry_eqc.erl
@@ -52,6 +52,9 @@ unregister(Name) ->
 unregister_pre(S,[Name]) ->
   lists:keymember(Name,1,S#state.regs).
 
+unregister_next(S,_,[Name]) ->
+  S#state{regs=lists:keydelete(Name,1,S#state.regs)}.
+
 %% the property
 
 prop_registry() ->
